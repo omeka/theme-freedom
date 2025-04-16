841f28b6cee4309fc57bc52e2bec73b378fdf6a7
Ken Albers <ken.albers@gmail.com>
2025-04-16 13:54:47 -0400

    Fix tags showing when no items are public

--- home/tagcloud.php
+++ home/tagcloud.php
@@ -18,6 +18,6 @@ if ($homepage_tagcloud_count === null || $homepage_tagcloud_count === '') {
 
 <?php if (get_theme_option('homepage_show_tagcloud')) : ?>
     <h2 class="title"><?php echo $homepage_tagcloud_title; ?></h2>
-    <?php $tags = get_records('Tag', array('sort_field' => 'count', 'sort_dir' => 'd'), $homepage_tagcloud_count); ?>
+    <?php $tags = get_records('Tag', array('type' => 'Item', 'sort_field' => 'count', 'sort_dir' => 'd'), $homepage_tagcloud_count); ?>
     <?php echo freedom_tag_cloud($tags, 'items/browse'); ?>
 <?php endif; ?>
