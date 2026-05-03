5f7fbe225407d99c7ffcdfcd68b70be7c50963fa
Nelson Amaya <nelsonamaya82@gmail.com>
2026-05-03 15:29:05 -0500

    Contribution pages adjustments

--- common/header.php
+++ common/header.php
@@ -43,7 +43,7 @@
     <!-- Stylesheets -->
     <?php
     queue_css_file(array('iconfonts','style'));
-    queue_css_url('//fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap');
+    queue_css_url('//fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap');
     queue_css_url('//fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap');
     
     queue_css_string(
